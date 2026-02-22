80edf758ec4b8a442d94744c0c8ff790e78108ad
minhduc1222 <ducminhcao2005@gmail.com>
2026-02-22 22:00:11 +0700

    add docker and docker-compose files

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const pool = new Pool({
    user: process.env.DB_USER || 'postgres',
    host: process.env.DB_HOST || 'localhost',
    database: process.env.DB_NAME || 'tododb',
-   password: process.env.DB_PASSWORD || 'postgres',
+   password: process.env.DB_PASSWORD || 'mypassword',
    port: process.env.DB_PORT || 5432,
 });
 
