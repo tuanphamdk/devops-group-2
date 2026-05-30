2cda585f5a00ce649d31c68e38bf1f2107e4d4b4
hnam <hainamtop1sv@gmail.com>
2026-05-30 22:54:48 +0700

    fix bug 1 attempt 1 + bug 2-6 attemp 2

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,10 @@ app.use(express.json());
 
 // BUG #1: Wrong default password - doesn't match docker-compose!
 const pool = new Pool({
-   user: process.env.DB_USER || 'postgres',
-   host: process.env.DB_HOST || 'localhost',
-   database: process.env.DB_NAME || 'tododb',
-   password: process.env.DB_PASSWORD || 'wrongpassword',
+   user: process.env.DB_USER || 'devops',
+   host: process.env.DB_HOST || 'postgres',
+   database: process.env.DB_NAME || 'mydb',
+   password: process.env.DB_PASSWORD || 'devops123',
    port: process.env.DB_PORT || 5432,
 });
 
@@ -42,7 +42,7 @@ app.post('/api/todos', async (req, res) => {
       // Return 400 status with error message if invalid
 
       if (!title || title.trim() === '') {
-         return res.status(400).json({success: false, message: 'Title is required'});
+         return res.status(400).json({error: 'Title is required'})
       }
 
       const result = await pool.query(
@@ -65,7 +65,7 @@ app.delete('/api/todos/:id', async (req, res) => {
       if (result.rowCount === 0) {
          return res.status(404).json({ message: 'Todo not found' });
       }
-      res.status(204).end
+      res.status(200).end()
    }catch (err) {
       res.status(500).json({ error: err.message });
    }
@@ -82,7 +82,12 @@ app.put('/api/todos/:id', async (req, res) => {
       if (!newTitle || newTitle.trim() === '') {
          return res.status(400).json({success: false, message: 'New title is required'});
       }
-      const result = await pool.query('UPDATE FROM todos SET title = $1, completed = $2 WHERE id = $3 RETURNING *', [newTitle, newCompleted, id]);
+      const result = await pool.query('UPDATE todos SET title = $1, completed = $2 WHERE id = $3 RETURNING *', [newTitle, newCompleted, id]);
+
+      if (result.rowCount === 0) {
+         return res.status(404).json({ error: 'Todo not found' });
+      }
+
       res.json(result.rows[0]);
    }catch (err) {
       res.status(500).json({ error: err.message });
@@ -97,6 +102,12 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
    console.log(`Backend running on port ${port}`);
 });
+if (process.env.NODE_ENV !== 'test'){
+   app.listen(port, () => {
+      console.log(`Backend running on port ${port}`);
+   })
+}
 
 // BUG #6: App not exported - tests can't import it!
 // STUDENT FIX: Export the app module
+module.exports = app;
